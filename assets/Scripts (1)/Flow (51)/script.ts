2e1a67c7572ce3c32f0a0aeb0f8b73e72051d255
Luiz Bernardo <luiz.bernardo@plataformatec.com.br>
2017-05-18 20:05:44 -0300

    Refactoring to reduce amount of code related to button behavior

--- assets/Scripts (1)/Flow (51)/script.ts
+++ assets/Scripts (1)/Flow (51)/script.ts
@@ -1,7 +1,9 @@
-class LoadGameBehavior extends Sup.Behavior {
+class StandardButtonBehavior extends Sup.Behavior {
   // flag to tell when the mouse hover the button
   isHover : boolean = false;
-  sound : boolean = true;
+  turnOffSound : boolean = false;
+  soundToPlay: string = "Toc";
+  turnOffLoadScene: boolean = false;
   sceneToLoad: string = "Menu";
 
   awake() {
@@ -10,10 +12,12 @@ class LoadGameBehavior extends Sup.Behavior {
 
   mouse(action) {
     if(action == "click"){
-      if (this.sound){
-        Sup.Audio.playSound("Sounds/Toc"); 
+      if (!this.turnOffSound){
+        Sup.Audio.playSound("Sounds/"+this.soundToPlay); 
       }
-      //Precisamos de uma função aqui para inicializar o jogo
+      if (!this.turnOffLoadScene){
+        Sup.loadScene("Scenes/"+this.sceneToLoad);
+      }
     }
     else if(action == "hover"){
       this.actor.spriteRenderer.setAnimation("hover");
@@ -42,54 +46,7 @@ class LoadGameBehavior extends Sup.Behavior {
 
   }
 }
-Sup.registerBehavior(LoadGameBehavior);
-
-
-class LoadSceneBehavior extends Sup.Behavior {
-  // flag to tell when the mouse hover the button
-  isHover : boolean = false;
-  sound : boolean = true;
-  sceneToLoad: string = "Menu";
-
-  awake() {
-    ray = new Sup.Math.Ray(this.actor.getPosition(), new Sup.Math.Vector3(0, 0, -1));
-  }
-
-  mouse(action) {
-    if(action == "click"){
-      if (this.sound){
-        Sup.Audio.playSound("Sounds/Toc"); 
-      }
-      Sup.loadScene("Scenes/"+this.sceneToLoad);
-    }
-    else if(action == "hover"){
-      this.actor.spriteRenderer.setAnimation("hover");
-    }
-    else if(action == "unhover"){
-      this.actor.spriteRenderer.setAnimation("unhover");
-    }
-  }
-
-  update() {
-    ray.setFromCamera(Sup.getActor("Camera").camera, Sup.Input.getMousePosition());
-
-    if(ray.intersectActor(this.actor, false).length > 0){
-      if(!this.isHover){
-        this.mouse("hover");
-        this.isHover = true;
-      }
-      if(Sup.Input.wasMouseButtonJustPressed(0)){
-        this.mouse("click")
-      }
-    }
-    else if(this.isHover){
-      this.isHover = false;
-      this.mouse("unhover")
-    }
-
-  }
-}
-Sup.registerBehavior(LoadSceneBehavior);
+Sup.registerBehavior(StandardButtonBehavior);
 
 
 class ExitOptionBehavior extends Sup.Behavior {
@@ -134,44 +91,3 @@ class ExitOptionBehavior extends Sup.Behavior {
 }
 Sup.registerBehavior(ExitOptionBehavior);
 
-
-
-class BackToMenuBehavior extends Sup.Behavior {
-  // flag to tell when the mouse hover the button
-  isHover : boolean = false;
-
-  awake() {
-    ray = new Sup.Math.Ray(this.actor.getPosition(), new Sup.Math.Vector3(0, 0, -1));
-  }
-
-  mouse(action) {
-    if(action == "click"){
-      Sup.loadScene("Scenes/Menu");
-    }
-    else if(action == "hover"){
-      this.actor.textRenderer.setColor(ColorTextDarkRed); 
-    }
-    else if(action == "unhover"){
-      this.actor.textRenderer.setColor(ColorTextRed);
-    }
-  }
-
-  update() {
-    ray.setFromCamera(Sup.getActor("Camera").camera, Sup.Input.getMousePosition());
-
-    if(ray.intersectActor(this.actor, false).length > 0){
-      if(!this.isHover){
-        this.mouse("hover");
-        this.isHover = true;
-      }
-      if(Sup.Input.wasMouseButtonJustPressed(0)){
-        this.mouse("click")
-      }
-    }
-    else if(this.isHover){
-      this.isHover = false;
-      this.mouse("unhover")
-    }
-  }
-}
-Sup.registerBehavior(BackToMenuBehavior);
